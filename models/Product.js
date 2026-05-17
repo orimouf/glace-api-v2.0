5d3fc86ea6731a662ac8d3006154385efd993365
orimouf <orim.ouf@gmail.com>
2026-05-17 01:33:09 +0100

    update product 16/05/2026 1.0

--- models/Product.js
+++ models/Product.js
@@ -5,6 +5,8 @@ const ProductSchema = new mongoose.Schema(
         id: { type: String, required: true, unique: false },
         name: { type: String, required: true, unique: true },
         price: { type: String, required: true },
+        purchasePrice: { type: String, required: true },
+        qtyGlobal: { type: String, default: "" },
         qty_par_one: { type: String, default: "" },
         image: { type: String, default: ""},
         status: { type: Boolean, default: false}
