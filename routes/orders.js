724b0067d5b05d8445d235331d80adcdffa75c89
orimouf <orim.ouf@gmail.com>
2026-05-29 22:19:06 +0100

    update orders 29/05/2026 1.0

--- routes/orders.js
+++ routes/orders.js
@@ -38,7 +38,9 @@ router.post("/", async (req, res) => { // , verify
     async function calculeProfit(productName, Qty, QtyItem, Price) {
         const products = await Product.find()
         const thisProduct = products.filter( elm => elm.name == productName )
-        console.log("thisProduct :  "+thisProduct.purchasePrice);
+        console.log("thisProduct :  "+ thisProduct);
+        console.log("thisProduct 1111 :  "+ thisProduct.purchasePrice);
+        console.log("thisProduct 1111 :  "+ thisProduct.name);
         console.log("thisProduct 2222:  "+Number(thisProduct.purchasePrice));
         
         const profit = Number(Qty) * Number(QtyItem) * (Number(Price) - Number(thisProduct.purchasePrice))
